6a6898af3a706e423dd6caf16f767f93d2a0f8c7
Eli Shani <elishani50@gmail.com>
2020-11-28 22:59:51 +0200

    create tar file

--- create_tar_files.py
+++ create_tar_files.py
@@ -2,15 +2,39 @@
 
 import tarfile
 import os.path
+import configparser
+config = configparser.RawConfigParser()
 
-def make_tarfile(output_filename, source_dir, app_version):
-    app_tar = output_filename + app_version + ".tgz"
-    print "\n*** Creating tar file '" + app_tar + "' from dirctory  '" + source_dir + "'\n"
-    with tarfile.open(app_tar, "w:gz") as tar:
-      tar.add(source_dir, arcname=os.path.basename(source_dir))
-      
-output_filename = "elishani"
-source_dir = "/tmp/eli"
-app_version = "10.2.3.4"
 
-make_tarfile (output_filename, source_dir, app_version)
+def make_tarfile(tar_directory, output_filename, source_dir, app_version):
+  app_tar = output_filename + "_" + app_version + ".tgz"
+  app_dir = tar_directory + "/" + source_dir
+  print "*** Creating tar file '" + app_tar + "' from dirctory  '" + app_dir + "'"
+  with tarfile.open(app_tar, "w:gz") as tar:
+    tar.add(app_dir, arcname=os.path.basename(app_dir))
+
+def main():
+  config_file = "/tmp/config.cfg"
+  
+  config.read(config_file)
+  app_dict = dict(config.items("APP_VERSION"))
+  dir_dict = dict(config.items("TAR_DIRECTORY")) 
+  tar_dict = dict(config.items("TAR_FILES"))
+    
+  # Get application version
+  for key in app_dict:
+    app_version = app_dict[key]
+  
+  # Get tar directory
+  for key in dir_dict:
+    tar_directory = dir_dict[key]
+
+  # Create tar files
+  print "\n***** START creating tar files\n"
+  for key in tar_dict:
+    make_tarfile (tar_directory, tar_dict[key], key, app_version)
+  print "\n***** END creating tar files"
+
+if __name__ == '__main__':
+  main()
+    
