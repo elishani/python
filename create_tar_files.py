f3fc3ff8c5c4c0205f9a45c5259565d394c1953b
ELi Shani <elishani50@gmail.com>
2020-12-05 17:48:48 +0200

    Update create_tar_files.py

--- create_tar_files.py
+++ create_tar_files.py
@@ -3,8 +3,8 @@
 import tarfile
 import os.path
 import configparser
-config = configparser.RawConfigParser()
-
+config = configparser.ConfigParser()
+# config = configparser.RawConfigParser()
 
 def make_tarfile(tar_directory, output_filename, source_dir, app_version):
   app_tar = output_filename + "_" + app_version + ".tgz"
@@ -15,21 +15,22 @@ def make_tarfile(tar_directory, output_filename, source_dir, app_version):
 
 def main():
   config_file = "/tmp/config.cfg"
-  
   config.read(config_file)
-  app_dict = dict(config.items("APP_VERSION"))
-  dir_dict = dict(config.items("TAR_DIRECTORY")) 
-  tar_dict = dict(config.items("TAR_FILES"))
-    
-  # Get application version
-  for key in app_dict:
-    app_version = app_dict[key]
+  app_ver = "application_version"
+  dir_tar = "tar_directory"
   
-  # Get tar directory
-  for key in dir_dict:
-    tar_directory = dir_dict[key]
+  app_ver = config['APP_VERSION'][app_ver]
+  dir_tar = config['TAR_DIRECTORY'][dir_tar]
 
   # Create tar files
+  print "\n***** START creating tar files\n"
+  for key in config['TAR_FILES']:
+    make_tarfile (dir_tar, config['TAR_FILES'][key], key, app_ver)
+  print "\n***** END creating tar files"
+
+if __name__ == '__main__':
+  main()
+
   print "\n***** START creating tar files\n"
   for key in tar_dict:
     make_tarfile (tar_directory, tar_dict[key], key, app_version)
@@ -37,4 +38,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-    
